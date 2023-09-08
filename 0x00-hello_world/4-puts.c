33ba8fbb3696dd399ab68d17492ccaf901939262
Nowoola-Y <nowoolayusuf@gmail.com>
2023-09-07 20:15:23 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#iclude < stdio.h >
+/**
+ * main -it prints out output using put
+ *
+ * Return: always (0)
+ *
+ */
+int main(voi)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
