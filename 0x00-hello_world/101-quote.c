b23d86fc897df795ad6f72dde68943ae6e25786f
Nowoola-Y <nowoolayusuf@gmail.com>
2023-09-11 15:08:40 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that print a line to the standar error
+ * Return 1 (Success)
+ */ 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
